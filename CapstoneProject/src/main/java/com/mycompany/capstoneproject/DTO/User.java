3fedbbbf53292714bb3794f15f2044e768dbe962
CStalder <christopher.jr.stalder@gmail.com>
2016-06-21 16:32:18 -0400

    Created User DTO

--- CapstoneProject/src/main/java/com/mycompany/capstoneproject/DTO/User.java
+++ CapstoneProject/src/main/java/com/mycompany/capstoneproject/DTO/User.java
@@ -5,10 +5,76 @@
  */
 package com.mycompany.capstoneproject.DTO;
 
+import java.util.Date;
+
 /**
  *
  * @author apprentice
  */
 public class User {
+    private int id;
+    private String name;
+    private String email;
+    private String password;
+    private String role;
+    private int numOfComments;
+    private Date joinedOn;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public int getNumOfComments() {
+        return numOfComments;
+    }
+
+    public void setNumOfComments(int numOfComments) {
+        this.numOfComments = numOfComments;
+    }
+
+    public Date getJoinedOn() {
+        return joinedOn;
+    }
+
+    public void setJoinedOn(Date joinedOn) {
+        this.joinedOn = joinedOn;
+    }
     
+   
 }
