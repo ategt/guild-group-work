527c0033ed018d5f710d19b0f07a62902d2ae4cf
CStalder <christopher.jr.stalder@gmail.com>
2016-06-21 16:29:54 -0400

    Created Static Page DTO

--- CapstoneProject/src/main/java/com/mycompany/capstoneproject/DTO/StaticPage.java
+++ CapstoneProject/src/main/java/com/mycompany/capstoneproject/DTO/StaticPage.java
@@ -10,5 +10,42 @@ package com.mycompany.capstoneproject.DTO;
  * @author apprentice
  */
 public class StaticPage {
+    private int id;
+    private String title;
+    private Image image;
+    private String content;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Image getImage() {
+        return image;
+    }
+
+    public void setImage(Image image) {
+        this.image = image;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+    
     
 }
