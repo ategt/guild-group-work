cb3ebd6b213bb97b0b5cd92942d16204f0180acd
Adam Tegtmeier <ategtmei@gmail.com>
2016-06-23 11:20:41 -0400

    WD-37 More unit tests.

--- CapstoneProject/src/test/java/com/mycompany/capstoneproject/DAO/BlogPostDaoDBImplTest.java
+++ CapstoneProject/src/test/java/com/mycompany/capstoneproject/DAO/BlogPostDaoDBImplTest.java
@@ -6,22 +6,19 @@
 package com.mycompany.capstoneproject.DAO;
 
 import com.mycompany.capstoneproject.DTO.BlogPost;
-import com.mycompany.capstoneproject.DTO.User;
 import java.util.Date;
-import org.junit.After;
-import org.junit.Before;
+import java.util.UUID;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  *
  * @author apprentice
  */
 public class BlogPostDaoDBImplTest {
-    
-//    ApplicationContext ctx;
+
+    ApplicationContext ctx;
 //
 //    public BlogPostDaoDBImplTest() {
 //        ctx = new ClassPathXmlApplicationContext("test-applicationContext.xml");
@@ -72,4 +69,120 @@ public class BlogPostDaoDBImplTest {
 //        assertEquals(expResult, result);
 //        
 //    }
+
+    @Test
+    public void testNullUpdate() {
+        System.out.println("delete");
+
+        BlogPostInterface blogPostDao = ctx.getBean("blogPostDao", BlogPostInterface.class);
+        UserInterface userDaoDBImpl = ctx.getBean("userDaoDBImpl", UserInterface.class);
+
+        BlogPost post = blogPostFactory();
+        BlogPost expResult = post;
+        BlogPost result = blogPostDao.create(post);
+        assertEquals(expResult, result);
+
+        int id = result.getId();
+        assertTrue(result.getId() != 0);
+        //assertTrue(result.getId() >= blogPostDao.size());
+
+        // Test get method.
+        BlogPost returnedPost = blogPostDao.getById(id);
+        assertTrue(verifyBlogPost(returnedPost, result));
+        blogPostDao.update(null);
+        blogPostDao.delete(post);
+        returnedPost = blogPostDao.getById(id);
+        assertEquals(returnedPost, null);
+
+        //This is a test update with nulls.
+        // If it makes it to here, it passed.
+        assertTrue(true);
+
+    }
+
+    private static Boolean verifyBlogPost(BlogPost blogPost1, BlogPost blogPost2) {
+        if (blogPost1 == null && blogPost2 == null) {
+            return true;
+        }
+
+        if (blogPost1 == null || blogPost2 == null) {
+            return false;
+        }
+
+        boolean valid = true;
+
+        if (!blogPost1.getTitle().equals(blogPost2.getTitle())) {
+            valid = false;
+        }
+
+        if (!blogPost1.getSlug().equals(blogPost2.getSlug())) {
+            valid = false;
+        }
+
+        if (!blogPost1.getContent().equals(blogPost2.getContent())) {
+            valid = false;
+        }
+
+        if (blogPost1.getId() != blogPost2.getId()) {
+            valid = false;
+        }
+
+        if (!isSameDay(blogPost1.getDateToPostOn(), blogPost2.getDateToPostOn()) ) {
+            valid = false;
+        }
+
+        if (!isSameDay(blogPost1.getExpireOn(), blogPost2.getExpireOn()) ) {
+            valid = false;
+        }
+
+        if (!isSameDay(blogPost1.getPostedOn(), blogPost2.getPostedOn()) ) {
+            valid = false;
+        }
+
+        return valid;
+    }
+
+    private static boolean isSameDay(java.util.Date date1, java.util.Date date2) {
+        if (date1 == null && date2 == null) {
+            return true;
+        }
+        java.text.SimpleDateFormat fmt = new java.text.SimpleDateFormat("yyyyMMdd");
+
+        if (date1 == null || date2 == null) {
+            return false;
+        }
+        return fmt.format(date1).equals(fmt.format(date2));
+    }
+
+    private BlogPost blogPostFactory() {
+
+        BlogPost blogPost = new BlogPost();
+        
+        Date dateToPostOn = new Date();
+        Date expireOn = new Date();
+        Date postedOn = new Date();
+        
+        UUID uuid = UUID.randomUUID();
+        String content = uuid.toString();
+        
+        String title = UUID.randomUUID().toString();
+        
+        
+//        blogPost.setAuthor(author);
+//        blogPost.setCategory(category);
+//        blogPost.setComments(comments);
+        blogPost.setContent(content);
+        blogPost.setDateToPostOn(dateToPostOn);
+        blogPost.setExpireOn(expireOn);
+        //blogPost.setHashTag(hashTag);
+        //blogPost.setId(0);
+        //blogPost.setImage(image);
+        blogPost.setPostedOn(postedOn);
+        //blogPost.setSlug(slug);
+        blogPost.setTitle(title);
+        
+        return blogPost;
+        
+    }
+
 }
