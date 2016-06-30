889f691d1a3c328826c7da56b9571e254c38dfd0
Adam Tegtmeier <ategtmei@gmail.com>
2016-06-30 11:32:49 -0400

    WD-85, Bug Fixed.

--- CapstoneProject/src/main/java/com/mycompany/capstoneproject/controller/ImageController.java
+++ CapstoneProject/src/main/java/com/mycompany/capstoneproject/controller/ImageController.java
@@ -11,6 +11,7 @@ import com.mycompany.capstoneproject.DAO.ImageInterface;
 import com.mycompany.capstoneproject.DTO.Category;
 import com.mycompany.capstoneproject.DTO.File;
 import com.mycompany.capstoneproject.DTO.Image;
+import com.mycompany.capstoneproject.bll.ImageServices;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -26,6 +27,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 import org.apache.commons.io.IOUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -45,13 +47,16 @@ import org.springframework.web.multipart.MultipartFile;
 @RequestMapping(value = "/image")
 public class ImageController {
 
+    @Autowired
     private ImageInterface imageDao;
 
-    @Inject
-    public ImageController(ImageInterface imageDao) {
-        this.imageDao = imageDao;
-    }
+    @Autowired
+    private ImageServices imageServices;
 
+//    @Inject
+//    public ImageController(ImageInterface imageDao) {
+//        this.imageDao = imageDao;
+//    }
     @RequestMapping(method = RequestMethod.GET, value = "/showimage/{id}")
     @ResponseBody
     public void getImage(@PathVariable("id") Integer id, HttpServletResponse response, HttpServletRequest request)
@@ -73,139 +78,89 @@ public class ImageController {
 
         List<Image> images = imageDao.list();
 
-        List<Integer> intList =  images.stream().map(Image::getId).collect(Collectors.toList());
-        
+        List<Integer> intList = images.stream().map(Image::getId).collect(Collectors.toList());
+
         return intList;
     }
 
-    
-    
-    
-    
     @RequestMapping(method = RequestMethod.GET, value = "/imagetest")
     public String imagePickerTest(Map model) {
 
         List<Image> images = imageDao.list();
 
-        List<Integer> imageIdList =  images.stream()
+        List<Integer> imageIdList = images.stream()
                 .filter(a -> a != null)
                 .filter(a -> a.getDescription() != null)
                 .filter(a -> a.getDescription().toLowerCase().contains("ajax"))
                 .map(Image::getId)
                 .collect(Collectors.toList());
-        
+
         model.put("imageIdList", imageIdList);
-        
+
         return "pickerTest";
     }
 
-    
-    
     @RequestMapping(method = RequestMethod.GET, value = "/imagetestb")
     public String imagePickerTestB(Map model) {
 
         List<Image> images = imageDao.list();
 
-        List<Integer> imageIdList =  images.stream()
+        List<Integer> imageIdList = images.stream()
                 .filter(a -> a != null)
                 .filter(a -> a.getDescription() != null)
                 .filter(a -> a.getDescription().toLowerCase().contains("ajax"))
                 .map(Image::getId)
                 .collect(Collectors.toList());
-        
+
         model.put("imageIdList", imageIdList);
-        
+
         return "pickerTestB";
     }
-    
+
     @RequestMapping(method = RequestMethod.GET, value = "/imageadmin")
     public String imageAdmin(Map model) {
 
         List<Image> images = imageDao.list();
 
-        List<Integer> imageIdList =  images.stream()
+        List<Integer> imageIdList = images.stream()
                 .filter(a -> a != null)
                 .filter(a -> a.getDescription() != null)
                 .filter(a -> a.getDescription().toLowerCase().contains("ajax"))
                 .map(Image::getId)
                 .collect(Collectors.toList());
-        
+
         model.put("imageIdList", imageIdList);
-        
+
         return "imageAdmin";
     }
-    
-    
-        
-    @RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
     @ResponseBody
     public void delete(@PathVariable("id") Integer contactId) {
-        
+
         Image image = imageDao.get(contactId);
-        
+
         imageDao.delete(image);
-        
+
     }
-    
-    
+
     @RequestMapping(method = RequestMethod.POST, value = "/imageadmin")
     public String fileUploaded(Model model, @Validated File file,
             BindingResult result) {
-
-        String filePath = "";
 
         String returnVal = "successFile";
         if (result.hasErrors()) {
             returnVal = "imageAdmin";
         } else {
-            InputStream inputStream = null;
+
             try {
                 MultipartFile multipartFile = file.getFile();
-                //multipartFile.
-                // IOUtils.copy(byteArrayInputStream, new FileOutputStream(outputFileName));
-
-                inputStream = multipartFile.getInputStream();
-
-                java.io.File fileImagesDir = new java.io.File("./uploadedImages");
-                if (!fileImagesDir.exists()) {
-                    fileImagesDir.mkdir();
-                }
-
-                java.io.File outputFile = new java.io.File("test.file");
-                filePath = outputFile.getAbsolutePath();
-                OutputStream outputStream = new FileOutputStream(outputFile);
-                IOUtils.copy(inputStream, outputStream);
-                model.addAttribute("filePath", filePath);
-
-                String originalName = multipartFile.getOriginalFilename();
-                String contentType = multipartFile.getContentType();
-                Long fileSize = multipartFile.getSize();
-                String multipartFileName = multipartFile.getName();
-
-                model.addAttribute("originalName", originalName);
-                model.addAttribute("contentType", contentType);
-                model.addAttribute("fileSize", fileSize);
-                model.addAttribute("multipartFileName", multipartFileName);
-
-                java.io.File saveFile = null;
-                if (fileImagesDir.isDirectory()) {
-                    saveFile = new java.io.File(fileImagesDir.getAbsolutePath() + "/" + originalName);
-                }
-
-                InputStream secondInputStream = multipartFile.getInputStream();
-                IOUtils.copy(secondInputStream, new FileOutputStream(saveFile));
-                String savedPath = saveFile.getAbsolutePath();
-                model.addAttribute("savedPath", savedPath);
-
+                Image image = imageServices.addImageToDatabase(multipartFile);
+                imageServices.loadRecentInfoIntoModel(model, image);
             } catch (IOException ex) {
-                Logger.getLogger(FileController.class.getName()).log(Level.SEVERE, null, ex);
-            } finally {
-                try {
-                    inputStream.close();
-                } catch (IOException ex) {
-                    Logger.getLogger(FileController.class.getName()).log(Level.SEVERE, null, ex);
-                }
+                Logger.getLogger(ImageController.class.getName()).log(Level.SEVERE, null, ex);
             }
+
         }
         return returnVal;
     }
@@ -227,5 +182,4 @@ public class ImageController {
 //        
 //        return "imagesAdmin";
 //    }
-    
 }
