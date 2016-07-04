75878e16d7fbfadac6766d9733e5ec07b5263f64
CStalder <christopher.jr.stalder@gmail.com>
2016-07-04 16:07:46 -0400

    Stuff

--- CapstoneProject/src/main/webapp/js/user.js
+++ CapstoneProject/src/main/webapp/js/user.js
@@ -73,26 +73,6 @@ $(document).ready(function () {
 
         });
     });
-
-//    $(document).on('click', '.delete-link', function (e) {
-//
-//        e.preventDefault();
-//
-//        var userId = $(e.target).data('user-id');
-//
-//        $.ajax({
-//            type: 'DELETE',
-//            url: contextRoot + '/user/' + userId,
-//            success: function (data, status) {
-//                $('#user-row-' + userId).remove();
-//            },
-//            error: function (data, status) {
-//                alert:('error');
-//            }
-//
-//        });
-//
-//    });
 });
 
 //});
