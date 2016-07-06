a0e941b1446e64e43903c41a71cbb8bf99cd3690
Adam Tegtmeier <ategtmei@gmail.com>
2016-07-06 17:55:42 -0400

    Last commit

--- UpdateWithTests.sh
+++ UpdateWithTests.sh
@@ -1,7 +1,7 @@
 mysql --user='root' --password='apprentice' --execute='CREATE DATABASE capstone_test;'
 mysql -h "localhost" -u "root" "-papprentice" "capstone_test" < "capstoneprewipe.sql"
 
-mysql -h "localhost" -u "root" "-papprentice" "capstone_test" < "capstone.sql"
+mysql -h "localhost" -u "root" "-papprentice" "capstone" < "capstone.sql"
 
 
 
