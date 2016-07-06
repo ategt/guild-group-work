d6d70caba99895d3b905f88f99a4cfdaef7ccca6
CStalder <christopher.jr.stalder@gmail.com>
2016-07-06 15:15:28 -0400

    Fixed update authorities table query

--- CapstoneProject/src/main/java/com/mycompany/capstoneproject/DAO/UserDaoDBImpl.java
+++ CapstoneProject/src/main/java/com/mycompany/capstoneproject/DAO/UserDaoDBImpl.java
@@ -25,7 +25,7 @@ public class UserDaoDBImpl implements UserInterface {
     private static final String SQL_INSERT_USER = "INSERT INTO user (name, role, password, email, num_of_comments, date_joined , enabled) VALUES (?, ?, ?, ?, ?, ?, ?)";
     private static final String SQL_INSERT_USER_AUTHORITY = "INSERT INTO `capstone`.`authorities` (`type`, `user_id`) VALUES (?, ?);";
     private static final String SQL_UPDATE_USER = "UPDATE user SET name = ?, role = ?, password = ?, email = ?, num_of_comments = ?, date_joined = ?, enabled = ? WHERE id = ?";
-    private static final String SQL_UPDATE_USER_AUTHORITY = "UPDATE `capstone`.`authorities` SET `type`= ? WHERE `id`= ?;";
+    private static final String SQL_UPDATE_USER_AUTHORITY = "UPDATE `capstone`.`authorities` SET `type`= ? WHERE `user_id`= ?;";
     private static final String SQL_SOFT_DELETE_USER = "UPDATE user SET enabled = 0 WHERE id = ?";
     private static final String SQL_DELETE_USER = "DELETE FROM user WHERE id = ?";
     private static final String SQL_DELETE_USER_AUTHORITY = "DELETE FROM capstone.authorities WHERE id = ?";
